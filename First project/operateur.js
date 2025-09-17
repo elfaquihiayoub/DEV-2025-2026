0cf5064f630dff26a820471efb88937c7ee94f3a
elfaquihiayoub <elfaquihi.ayoub@gmail.com>
2025-09-17 13:34:19 +0100

    change the if stetments by switch

--- First project/operateur.js
+++ First project/operateur.js
@@ -5,19 +5,18 @@ let subs=Num1-Num2
 let multi=Num1*Num2
 let div=Num1/Num2
 let choix=prompt('1 pour somme , 2 pour substraction , 3 pour multiplication , 4 pour division')
-if (choix==1) {
-    alert(somme)
-    
-}
-if(choix==2){
-    alert(subs)
-
-}
-if(choix==3){
-    alert(multi)
-
-}
-if(choix==4){
-    alert(div)
-
+switch (choix) {
+    case '1':
+        alert(somme)
+        break;
+    case '2':
+        alert(subs)
+        break;
+    case '3':
+        alert(multi)
+        break;
+    case '4':
+        alert(div)
+        break;
+                        
 }
