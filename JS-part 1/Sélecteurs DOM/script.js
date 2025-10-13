572c3be4677132d0d1439e01efd1b382f302e109
elfaquihi_ayoub <elfaquihi.ayoub@gmail.com>
2025-10-13 16:09:31 +0100

    adding selectors section

---
+++ JS-part 1/Sélecteurs DOM/script.js
@@ -0,0 +1,6 @@
+let title = document.getElementById("titre-principal")
+console.log("title ="+ title.innerText)
+
+let button= document.querySelector(".btn")
+console.log("button =" + button.innerText) 
+
