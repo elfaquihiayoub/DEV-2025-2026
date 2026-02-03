fb33204ef4ddc1a5dd2133889230012760db111d
elfaquihi_ayoub <elfaquihi.ayoub@gmail.com>
2026-02-03 11:52:26 +0100

    ..

--- PHP/Chapitre 2 – Conditions if, else, switch/conditions.php
+++ PHP/Chapitre 2 – Conditions if, else, switch/conditions.php
@@ -3,34 +3,35 @@
 // if statement
 
 $count_jour_desEmprunt=25;
-$jour="";
+$jour="vendredi";
 
 
-if($count_jour_desEmprunt>30 ){
-    echo "you passed the emprunt limite !!!";
-}
-elseif ($count_jour_desEmprunt>20) {
-    echo "you are in your last emprunt days!";
-}
+// if($count_jour_desEmprunt>30 ){
+//     echo "you passed the emprunt limite !!!";
+// }
+// elseif ($count_jour_desEmprunt>20) {
+//     echo "you are in your last emprunt days!";
+// }
 
-else{
-    echo "enjoy your read and dont surpass the limite ";
+// else{
+//     echo "enjoy your read and dont surpass the limite ";
 
-}
+// }
 
 
 // switch statement 
 
 switch($jour){
     case "lundi":
-        echo "first day of the week";
+        echo "first day of the week <br>";
         break;
     
     case "vendredi":
-        echo "lest day of the week"  ;
+        echo "last day of the week <br>"  ;
+        break;
         
     default:
-    echo "normal day ";    
+    echo "normal day <br>";    
 }
 
 
